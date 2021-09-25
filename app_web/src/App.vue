80ed9347f1cf093ac4d0fd8929510bd1028d5f54
kevinstif <kevin_stif_2012@hotmail.com>
2021-09-25 17:56:07 -0500

    nav-bar_v1

--- app_web/src/App.vue
+++ app_web/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+      <div>
+      <v-img :src="require('./img/Logo.png')"/>
+      </div>
+      <v-spacer></v-spacer>
+      <h3 >User Name</h3>
+      <v-btn icon>
+        <v-img  class="" src="#"></v-img>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
